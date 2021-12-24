82f5c684df9901b13e6cd22c5dd1d7d6b8c38160
tuchongyang <779311998@qq.com>
2021-12-24 18:05:05 +0800

    update config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("@vue/cli-service")
 module.exports = defineConfig({
   transpileDependencies: true,
+  productionSourceMap: false,
   css: {
     loaderOptions: {
       sass: {
